1fdac13ca2fd2dd832856c1095b8e603a3bcc3ea
Anton <71523607+AnToxa566@users.noreply.github.com>
2023-05-06 14:38:19 +0300

    fix: Delete PHPMailer

--- send_mail.php
+++ send_mail.php
@@ -1,21 +1,5 @@
 <?php
 
-// use PHPMailer\PHPMailer\PHPMailer;
-// use PHPMailer\PHPMailer\Exception;
-
-// require 'PHPMailer/src/PHPMailer.php';
-// require 'PHPMailer/src/Exception.php';
-
-// $mail = new PHPMailer(true);
-// $mail->CharSet = "UTF-8";
-
-// $mail->addAddress("bogachuk566@gmail.com");
-// $mail->Subject = "[Заявка с формы]";
-// $mail->Body = $_POST['email'] . ' ' . $_POST['request'];
-
-// $mail->send();
-// header('location: index.html');
-
 $email = $_POST['email'];
 $request = $_POST['request'];
 
