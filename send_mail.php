b7139674dd70ab5fa3166479626da66927aeb094
Anton <71523607+AnToxa566@users.noreply.github.com>
2023-05-06 14:33:54 +0300

    fix: Error send

--- send_mail.php
+++ send_mail.php
@@ -1,17 +1,27 @@
 <?php
 
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\Exception;
+// use PHPMailer\PHPMailer\PHPMailer;
+// use PHPMailer\PHPMailer\Exception;
 
-require 'PHPMailer/src/PHPMailer.php';
-require 'PHPMailer/src/Exception.php';
+// require 'PHPMailer/src/PHPMailer.php';
+// require 'PHPMailer/src/Exception.php';
 
-$mail = new PHPMailer(true);
-$mail->CharSet = "UTF-8";
+// $mail = new PHPMailer(true);
+// $mail->CharSet = "UTF-8";
 
-$mail->addAddress("bogachuk566@gmail.com");
-$mail->Subject = "[Заявка с формы]";
-$mail->Body = $_POST['email'] . ' ' . $_POST['request'];
+// $mail->addAddress("bogachuk566@gmail.com");
+// $mail->Subject = "[Заявка с формы]";
+// $mail->Body = $_POST['email'] . ' ' . $_POST['request'];
 
-$mail->send();
-header('location: index.html');
+// $mail->send();
+// header('location: index.html');
+
+$email = $_POST['email'];
+$request = $_POST['request'];
+
+if (mail("bogachuk566@gmail.com", $email . ' ' . $request, $email)) {
+    echo "Completed!";
+}
+else {
+    echo "Failed!";
+}
